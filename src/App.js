a36f73ee778042e2d540cff5c8957e9a017020d5
William Violette <violettewj@gmail.com>
2020-03-24 09:59:28 -0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect, useRef} from 'react';
+import data from './data/data.json'
+import bar from './bar'
+import * as d3 from 'd3'
+
+let container = d3.select('.chart-container');
+let barChart = bar();
+let dataset = [...data];
+
+barChart
+    .width(300)
+    .height(200)
+    .margin({
+        left: 50,
+        bottom: 30
+    })
+    .on('customMouseOver', function(event, data) {
+        console.log('data', data);
+    });
+
+container.datum(dataset).call(barChart);
 
 function App() {
+
+  useEffect(()=>{
+    console.log(container)
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="chart-container">
+      
     </div>
   );
 }
